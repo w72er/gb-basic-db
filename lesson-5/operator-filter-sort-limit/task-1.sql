7f0e1bd7fa6ddd5cd3b6cd68232de2b4a57e6400
Alexander Makarevich <alexander.makarevich@eltex.loc>
2021-07-15 13:24:09 +0700

    lesson 5 task-1-1

---
+++ lesson-5/operator-filter-sort-limit/task-1.sql
@@ -0,0 +1,45 @@
+--
+-- Short answer:
+--
+UPDATE IGNORE users
+SET created_at = NOW()
+WHERE created_at IS NULL;
+
+UPDATE IGNORE users
+SET updated_at = NOW()
+WHERE updated_at IS NULL;
+
+--
+-- Solution
+--
+DROP DATABASE IF EXISTS lesson5;
+CREATE DATABASE lesson5;
+USE lesson5;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT NULL,
+    updated_at DATETIME DEFAULT NULL
+);
+
+INSERT users VALUES
+    (DEFAULT, 'Fred', NULL, NULL),
+    (DEFAULT, 'Smith', now() - interval 30 year,  now() - interval 20 year ),
+    (DEFAULT, 'Mark', NULL, now() - interval 30 year);
+
+SELECT * FROM users;
+
+SELECT * FROM users
+WHERE created_at IS NULL OR updated_at IS NULL;
+
+UPDATE users
+SET created_at = NOW()
+WHERE created_at IS NULL;
+
+UPDATE users
+SET updated_at = NOW()
+WHERE updated_at IS NULL;
+
+SELECT * FROM users
+WHERE created_at IS NULL OR updated_at IS NULL;
