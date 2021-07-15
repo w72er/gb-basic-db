6b6a649302a5c7cd5a78cf0ce6f68b9123976fb4
Alexander Makarevich <alexander.makarevich@eltex.loc>
2021-07-15 13:25:10 +0700

    lesson 5 task-1-1

--- lesson-5/operator-filter-sort-limit/task-1.sql
+++ lesson-5/operator-filter-sort-limit/task-1.sql
@@ -1,4 +1,9 @@
 --
+-- 1. Пусть в таблице users поля created_at и updated_at оказались незаполненными.
+-- Заполните их текущими датой и временем.
+--
+
+--
 -- Short answer:
 --
 UPDATE IGNORE users
