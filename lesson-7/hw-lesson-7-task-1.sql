e7914301689caae28aaab090a21c424f05ec797a
w72er <>
2021-07-22 21:52:40 +0700

    lesson 7 task 1

---
+++ lesson-7/hw-lesson-7-task-1.sql
@@ -0,0 +1,19 @@
+-- 1. Составьте список пользователей users,
+-- которые осуществили хотя бы один заказ orders
+-- в интернет магазине.
+
+-- INSERT orders (user_id) VALUES (1), (4);
+-- INSERT orders (user_id) VALUES (1), (4), (3);
+
+-- Solution 1:
+-- nested request
+SELECT name
+FROM users
+WHERE id = ANY (SELECT DISTINCT user_id FROM orders);
+-- WHERE id IN (SELECT DISTINCT user_id FROM orders);
+
+-- Solution 2
+-- use JOIN and uniq by DISTINCT
+SELECT DISTINCT users.name
+FROM orders
+LEFT JOIN users ON (orders.user_id = users.id);
