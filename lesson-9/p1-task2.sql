6e5d477815cd04ff29c0abbd4cf126edcd08b4d5
w72er <>
2021-07-30 13:18:53 +0700

    lesson 9 hw p 1 task 2

--- lesson-9/p1-task2.sql
+++ lesson-9/p1-task2.sql
@@ -5,6 +5,17 @@
 -- позиции из таблицы products и соответствующее название каталога
 -- name из таблицы catalogs.
 
+USE shop;
 
+SELECT products.name, catalogs.name
+FROM products
+LEFT JOIN catalogs ON products.catalog_id = catalogs.id;
 
+DROP VIEW IF EXISTS short_products;
 
+CREATE VIEW short_products (product, catalog)
+AS SELECT products.name AS product, catalogs.name AS catalog
+FROM products
+LEFT JOIN catalogs ON products.catalog_id = catalogs.id;
+
+SELECT * FROM short_products;
