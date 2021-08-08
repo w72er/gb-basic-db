bc97eae9496ce29e3d6c40512caa947f4bad59b3
w72er <>
2021-08-08 13:58:30 +0700

    project solution

--- project/artifacts/print-portfolio-cost.sql
+++ project/artifacts/print-portfolio-cost.sql
@@ -69,3 +69,56 @@ SELECT
 FROM tickers
 	INNER JOIN deals ON tickers.id = deals.ticker_id
     INNER JOIN prices ON tickers.id = prices.ticker_id;
+
+/* Вывести стоимость портфеля ver 3 */
+SELECT
+	*,
+    tickers.name AS ticker,
+    deals.amount AS amount,
+    prices.price AS price,
+    deals.amount * prices.price AS "sum"
+FROM tickers
+	INNER JOIN deals ON tickers.id = deals.ticker_id
+    INNER JOIN prices ON tickers.id = prices.ticker_id;
+
+/* Вывести стоимость портфеля ver 4 */
+SELECT
+	*,
+    tickers.name AS ticker,
+    deals.amount AS amount,
+    prices.price AS price,
+    deals.amount * prices.price AS "sum",
+    sum(deals.amount * prices.price) AS "sum2"
+FROM tickers
+	INNER JOIN deals ON tickers.id = deals.ticker_id
+    INNER JOIN prices ON tickers.id = prices.ticker_id
+GROUP BY tickers.id WITH ROLLUP;
+
+/* Вывести стоимость портфеля ver 5 */
+SELECT
+    tickers.name AS ticker,
+    deals.amount AS amount,
+    prices.price AS price,
+    sum(deals.amount * prices.price) AS "sum"
+FROM tickers
+	INNER JOIN deals ON tickers.id = deals.ticker_id
+    INNER JOIN prices ON tickers.id = prices.ticker_id
+GROUP BY ticker WITH ROLLUP;
+-- вывод не очень ясный, поэтому можно отдать вычисление на уровень приложения.
+-- тем более выводить придется все тикеры, который будет не более 10.
+
+-- Улучшим вариант без ROLLUP
+/* Вывести стоимость портфеля ver 6 */
+SELECT
+    tickers.name AS ticker,
+    deals.amount AS amount,
+    prices.price AS price,
+    deals.amount * prices.price AS "sum"
+FROM tickers
+	INNER JOIN deals ON tickers.id = deals.ticker_id
+    INNER JOIN prices ON tickers.id = prices.ticker_id;
+    
+-- Проверим результат: сумма должна быть около 30000 рублей.
+-- 3200 + 18000 + 8932 = 30132 - больше получилось из-за округлений
+-- при вычислении количества бумаг. Поэтому будем считать результат
+-- верным.
